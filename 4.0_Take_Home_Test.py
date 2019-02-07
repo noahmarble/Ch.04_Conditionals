97582eccda0c5e34ee7c438c5a933fb703a21ebf
noahmarble <mablenoa@urbandaleschools.com>
2019-02-07 12:43:55 -0600

    copied

--- 4.0_Take_Home_Test.py
+++ 4.0_Take_Home_Test.py
@@ -1,17 +1,23 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: Noah mable.
+but I will definitely not ask another person except the instructor. Signed: Noah Mabl
 
 
   1. What is missing from this code?
-     
+
      midichlorians = float(input("Enter midichlorian count: ")
      if midichlorians > 10000:
          print("You have serious Jedi potential")
      else:
          print("Jedi, you will never be.")
 '''
-     
+
+midichlorians = float(input("Enter midichlorian count: ")
+if  10000 < midichlorians:
+   print("You have serious Jedi potential")
+
+else:
+ print("Jedi, you will never be.")
      
      
 '''
