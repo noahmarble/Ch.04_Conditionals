7f51f967b6a8a28fb525d54acf4a7aab1c67621a
noahmarble <mablenoa@urbandaleschools.com>
2019-02-07 12:45:36 -0600

    copied

--- 4.1_Number_Analysis.py
+++ 4.1_Number_Analysis.py
@@ -6,15 +6,34 @@ Create a program that asks the user for a number and then analyzes it to determi
 2.) positive, negative or zero
 3.) inclusively between -100 and +100
 
-A small report will then be printed. Use the following to test your program:
+A small report will then be printed. Use the following to test your program
 
-In: 32  
+In: 32
 Out:  Test 1: Even
       Test 2: Positive
       Test 3: Inclusive
 
-In: -123  
+In: -123
 Out:  Test 1: Odd
       Test 2: Negative
       Test 3: Exclusive
 '''
+number=int(input("input a number:"))
+
+evenorodd = number%2                    #even or odd test
+if evenorodd == 1:
+    print("test 1: od")
+else:
+    print("test 1: even")
+                                         # pos, neg, or zero test
+if number == 0:
+    print("test 2: zero")
+elif number < 0:
+    print("test 2: negative")
+elif number > 0:
+    print("test 2: positive")
+                                    #inclusively between -100 and 100 test
+if -101 < number < 101:
+    print("test 3: inclusive")
+else:
+    print("test 3: exclusive")
--- 4.2_Quiz_Master.py
+++ 4.2_Quiz_Master.py
@@ -1,6 +1,75 @@
 '''
-QUIZ MASTER PROJECT
+QUIZ MASTER PROJEC
 -------------------
-The criteria for the project are on the website. Make sure you test this quiz with 
+The criteria for the project are on the website. Make sure you test this quiz with
 two of your student colleagues before you run it by your instructor.
 '''
+
+score = 0
+
+
+print("this is a test")
+
+#1. how many letters are in the word'letters'
+numoneanswer = input("1. how many letters are in the word'letters'\n\r   A. 1\n\r   B. 7\n\r   C. 6\n\r   Your Answer:")
+if numoneanswer.lower() == "a" or numoneanswer == "1":
+    print("correct")
+    score+=1
+elif numoneanswer.lower() == "b" or numoneanswer == "2":
+    print("wrong, trick question")
+elif numoneanswer.lower() == "c" or numoneanswer == "3":
+    print("wrong, trick question")
+else:
+    print("wrong")
+
+#2. when will the wall be built
+numtwoanswer = input("\n\r2. when will the wall be built?\n\r   A. 2019\n\r   B. 2020\n\r   C. 2021\n\r   Your Answer:")
+if numtwoanswer.lower() == "a" or numtwoanswer.lower() == "when will the wall be built":
+    print("nope, the goverment shut down will continue through trumps presidensy")
+elif numtwoanswer.lower() == "b" or numtwoanswer == "2020":
+    print("nope, we wont even have flying cars")
+elif numtwoanswer.lower == "c" or numtwoanswer == "2021":
+    print("who knows?")
+    score += 1
+else:
+    print("wrong")
+
+#3. how to comment
+numthreeanswer = input("\n\r3. how do you enter a single line comment?\n\r   A. idk\n\r   B. #this is how\n\r   C. //this is how\n\r   Your Answer:")
+if numthreeanswer.lower() == "a" or
+    print("wrong")
+elif numthreeanswer.lower() == "b":
+    print("correct")
+    score += 1
+elif numthreeanswer.lower() == "c":
+    print("wrong, this is NOT C sharp")
+else:
+    print("wrong")
+
+#4. how was python named
+numfouranswer = input("\n\r4. how was python named?\n\r   A. snake was the first game made in the language\n\r   B. the movie: mighty python and the holly grail\n\r   C. snakes are cool\n\r   Your Answer:")
+if numfouranswer.lower() == "a":
+    print("nope")
+elif numfouranswer.lower() == "b":
+    print("correct")
+    score += 1
+elif numfouranswer.lower() == "c":
+    print("no, snakes are not cool")
+else:
+    print("wrong")
+
+#5. what do you prefer
+numfiveanswer = input("\n\r5. what do you prefer\n\r   A. this quiz to be over\n\r   B. to own a zoo\n\r   C. a slice of pie\n\r   D. to get a high score\n\r   Your Answer:")
+if numfiveanswer.lower() == "a":
+    print("same, but wrong")
+elif numfiveanswer.lower() == "b":
+    print("what, why?")
+elif numfiveanswer.lower() == "c":
+    print("sounds yummy, but im a computer not a cook")
+elif numfiveanswer.lower == "d":
+    print("correct")
+    score += 1
+else:
+    print("wrong")
+
+print("\n\ryour score is:", score, "out of 5")
