0b086e0121404f67ad3d53c0c382431dd2b191a0
noahmarble <mablenoa@urbandaleschools.com>
2019-02-14 11:55:15 -0600

    all done

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
+print("Welcome to the Jedi Academy!")
 
+print("A. Jedi Master")
+print("B. Sith Lord")
+print("C. Droid")
 
-print("?" < "!")
+user_input = input("Choose a character?")
+
+if user_input.upper() == "A" or user_input.lower() == "jedi master":
+    sensitivity = 1000
+elif user_input.upper() == "B" or user_input.lower() == "sith lord":
+    sensitivity = 900
+elif user_input.upper() == "C" or user_input.lower() == "droid":
+    sensitivity = 0
+
+print("sensativity is:",sensitivity)
